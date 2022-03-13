105fa11e114f41177f110fe70c35ba7b1587f550
Tyas R <tyasrusmin@gmail.com>
2022-03-13 22:08:21 +0700

    remove comment

--- main.go
+++ main.go
@@ -8,11 +8,6 @@ import (
 )
 
 func main() {
-	// fmt.Println("testing aja")
-	// numb := int64(10)
-	// strconvert := strconv.Itoa(int(numb))
-	// fmt.Println("string convert from integer: ", strconvert)
-
 	user := entity.User{
 		ID:   0,
 		Name: "testing name user",
